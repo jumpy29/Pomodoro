a8d61ecf4ad1e09ffdf4217cb096252f08e21e80
jumpy <smitamin05@gmail.com>
2024-11-25 15:57:55 -0800

    improved start button design, removed reset button

--- timer_gui.py
+++ timer_gui.py
@@ -13,7 +13,7 @@ class TimerApp(QWidget):
         self.sound_filename = "timer_end.mp3"
         
         self.setWindowTitle("Pomodoro Timer")
-        self.setGeometry(100, 100, 300, 200)
+        self.setGeometry(0, 0, 350, 250)
         
         # Create a Timer instance with 10 seconds for testing
         self.timer = Timer(1)
@@ -30,31 +30,34 @@ class TimerApp(QWidget):
         self.layout = QVBoxLayout()
         self.setStyleSheet("""
             background: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 #fae1dd, stop:1 #f8edeb);
-            border-radius: 15px;
+            border-radius: 5px;
             color: #495057;
         """)
         # Time Label
         self.time_label = QLabel(f"{self.timer.format_time(self.timer.focus_time)}", self)
         self.layout.addWidget(self.time_label)
 
-        #font for time
+        # Font for time
         self.time_label.setFont(QFont("Courier New", 48, QFont.Weight.Bold))
-        self.time_label.setAlignment(Qt.AlignmentFlag.AlignCenter) #aligning to center
+        self.time_label.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Aligning to center
         
         # Start Button
         self.start_button = QPushButton("Start", self)
         self.start_button.clicked.connect(self.start_timer)
-        self.layout.addWidget(self.start_button)
+        self.layout.addWidget(self.start_button, alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        self.start_button.setFixedSize(100, 40)
+        self.start_button.setStyleSheet(
+            """
+            QPushButton{
+            background: #495057;
+            color: #fae1dd
+            }
+        """)
         
         # Stop Button
         self.stop_button = QPushButton("Stop", self)
         self.stop_button.clicked.connect(self.stop_timer)
         self.layout.addWidget(self.stop_button)
-        
-        # Reset Button
-        self.reset_button = QPushButton("Reset", self)
-        self.reset_button.clicked.connect(self.reset_timer)
-        self.layout.addWidget(self.reset_button)
         
         # Set layout
         self.setLayout(self.layout)
@@ -70,14 +73,11 @@ class TimerApp(QWidget):
     def stop_timer(self):
         self.timer.stop_timer()
 
-    def reset_timer(self):
-        self.timer.stop_timer()
+    def on_timer_finished(self):
+        self.timer.stop_timer()  # Stop the timer when finished
         self.timer.time_left = self.timer.focus_time  # Reset to initial time
         formatted_time = self.timer.format_time(self.timer.time_left)
-        self.time_label.setText(f"{formatted_time}") 
-    
-    def on_timer_finished(self):
-        self.reset_timer()
+        self.time_label.setText(f"{formatted_time}")
         self.play_sound()
 
     def play_sound(self):
